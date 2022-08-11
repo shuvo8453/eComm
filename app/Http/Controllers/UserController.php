689b3eac89ce46d1661adb568b8d25f861eb16b9
shuvo8453 <saifulshuvo2@gmail.com>
2022-08-11 21:44:13 +0600

    completing controller and session

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    //
+    function login(Request $req)
+    {
+        $user = User::where(['email'=>$req->email])->first();
+        if(!$user || !Hash::check($req->password,$user->password))
+        {
+            return "Email or Password not matched";
+        }else{
+            $req->session()->put('user',$user);
+            return redirect('/');
+        }
+    }
+}
