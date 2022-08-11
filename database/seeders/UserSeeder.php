84ba7e8b4a9187f8fbc74c0cfc15a96e83ad0d43
shuvo8453 <saifulshuvo2@gmail.com>
2022-08-11 19:14:31 +0600

    Test the git push

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,10 +15,10 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // 
         DB::table('users')->insert([
-            'name' => 'daul',
-            'email' => 'daul@daul.com',
+            'name' => 'Arup',
+            'email' => 'arup@gmail.com',
             'password' => Hash::make('12345')
         ]);
     }
